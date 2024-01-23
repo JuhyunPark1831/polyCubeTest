f08d11f7ed4244619a2867711d5e6309adf19d77
JuhyunPark1831 <epahs1831@naver.com>
2024-01-23 12:51:44 +0900

    init

---
+++ src/main/java/com/example/polycubeTest/PolycubeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.polycubeTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolycubeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolycubeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/polycubeTest/PolycubeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polycubeTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolycubeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
